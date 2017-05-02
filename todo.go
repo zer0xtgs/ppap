b5c6fd51301e3b49dd02ee285be40293120da9c2
Alexander <zer0xtgs@gmail.com>
2017-05-02 15:18:15 +0300

    first commit

---
+++ todo.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"database/sql"
+	_ "net/http"
+	"ppap/handlers"
+
+	"github.com/labstack/echo"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db := initDB("storage.db")
+	migrate(db)
+
+	e := echo.New()
+
+	e.File("/", "public/index.html")
+	e.GET("/tasks", handlers.GetTasks(db))
+	e.PUT("/tasks", handlers.PutTask(db))
+	e.DELETE("/tasks/:id", handlers.DeleteTask(db))
+
+	e.Logger.Fatal(e.Start(":1324"))
+}
+
+func initDB(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if db == nil {
+		panic("db nil")
+	}
+	return db
+}
+
+func migrate(db *sql.DB) {
+	sql := `
+	CREATE TABLE IF NOT EXISTS tasks(
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		name VARCHAR NOT NULL
+	);
+	`
+
+	_, err := db.Exec(sql)
+	if err != nil {
+		panic(err)
+	}
+}
